3c790ff54f4b6bf4a7e5afc89f76c7531fd0cd40
Ewelina Kociolek <ewelina.kociolek@wp.pl>
2023-02-10 21:11:11 +0100

    working game

--- main.js
+++ main.js
@@ -1,22 +1,76 @@
 import './sass/main.scss'
-const rows = 40;
-const cols = 40;
-let started=false;// Set to true when use clicks start
-let timer;//To control evolutions
-let evolutionSpeed=1000;// One second between generations
-// Need 2D arrays. These are 1D
-let currGen =[rows];
-let nextGen =[rows];
 
-const startButton = document.querySelector('#btnstartstop');
-// Creates two-dimensional arrays
+const rowsInput = document.getElementById("rows-input");
+const columnsInput = document.getElementById("cols-input");
+
+rowsInput.addEventListener("change", (event) => {
+	rowsInput.setAttribute("value", event.target.value);
+	rows = parseInt(event.target.value);
+});
+
+columnsInput.addEventListener("change", (event) => {
+	columnsInput.setAttribute("value", event.target.value);
+	cols = parseInt(event.target.value);
+});
+
+let rows = rowsInput.getAttribute("value");
+let cols = columnsInput.getAttribute("value");
+
+let started = false;
+let timer;
+let evolutionSpeed = 250;
+
+let currGen = [rows];
+let nextGen = [rows];
+
+const createButton = document.getElementById("create-button");
+createButton.addEventListener("click", () => {
+    document.getElementById("world").innerHTML = '';
+    createWorld();
+    createGenArrays();
+    initGenArrays();
+    console.log("Game board ready!");
+});
+
+const startButton = document.querySelector("#start-button");
+startButton.addEventListener("click", () => {
+    let startStop = document.querySelector("#start-button");
+    if (!started) {
+        started = true;
+        startStop.innerHTML = "Stop";
+        console.log("Let's play!");
+        evolve();
+    } else {
+        started = false;
+        startStop.innerHTML = "Start";
+        console.log("Game stopped!");
+        clearTimeout(timer);
+    }
+});
+
+const resetButton = document.querySelector("#reset-button");
+resetButton.addEventListener("click", () => {
+    location.reload();
+    console.log("Reload...");
+});
+
+const randomButton = document.querySelector("#random-button");
+randomButton.addEventListener("click", () => {
+    createWorld();
+    createGenArrays();
+    initGenArrays();
+    document.getElementById("world").innerHTML = '';
+    random();
+    console.log("Random cells are alive.");
+});
+
 function createGenArrays() {
     for (let i = 0; i < rows; i++) {
         currGen[i] = new Array(cols);
         nextGen[i] = new Array(cols);
-      
     }
 }
+
 function initGenArrays() {
     for (let i = 0; i < rows; i++) {
         for (let j = 0; j < cols; j++) {
@@ -25,190 +79,157 @@ function initGenArrays() {
         }
     }
 }
+
 function createWorld() {
-    let world = document.querySelector('#world');
-    
-    let tbl = document.createElement('table');
-    tbl.setAttribute('id','worldgrid');
-for (let i = 0; i < rows; i++) {
-        let tr = document.createElement('tr');
+    let world = document.querySelector("#world");
+    let tbl = document.createElement("table");
+    tbl.setAttribute("id", "worldgird");
+    tbl.setAttribute("class", "world__table");
+    for (let i = 0; i < rows; i++) {
+        let tr = document.createElement("tr");
         for (let j = 0; j < cols; j++) {
-            let cell = document.createElement('td');
-            cell.setAttribute('id', i + '_' + j);
-            cell.setAttribute('class', 'dead');
-            cell.addEventListener('click',cellClick);            
+            let cell = document.createElement("td");
+            cell.setAttribute("id", i + "_" + j);
+            cell.setAttribute("class", "dead");
+            cell.addEventListener("click", cellClick);
+
             tr.appendChild(cell);
         }
         tbl.appendChild(tr);
     }
     world.appendChild(tbl);
 }
+
 function cellClick() {
     let loc = this.id.split("_");
     let row = Number(loc[0]);
     let col = Number(loc[1]);
-// Toggle cell alive or dead
-    if (this.className==='alive'){
-        this.setAttribute('class', 'dead');
+    if (this.className === "alive") {
+        this.setAttribute("class", "dead");
+        console.log(`Cell ${this.id} is dead now.`);
         currGen[row][col] = 0;
-    }else{
-        this.setAttribute('class', 'alive');
+    } else {
+        this.setAttribute("class", "alive");
         currGen[row][col] = 1;
+        console.log(`Cell ${this.id} is alive now.`);
     }
 }
-function createNextGen() {
+
+function getNeighborCount(row, col) {
+	let count = 0;
+	let nrow = Number(row);
+	let ncol = Number(col);
+	if (nrow - 1 >= 0) {
+		if (currGen[nrow - 1][ncol] === 1) count++;
+	}
+	if (nrow - 1 >= 0 && ncol - 1 >= 0) {
+		if (currGen[nrow - 1][ncol - 1] === 1) count++;
+	}
+	if (nrow - 1 >= 0 && ncol + 1 < cols) {
+		if (currGen[nrow - 1][ncol + 1] === 1) count++;
+	}
+	if (ncol - 1 >= 0) {
+		if (currGen[nrow][ncol - 1] === 1) count++;
+	}
+	if (ncol + 1 < cols) {
+		if (currGen[nrow][ncol + 1] === 1) count++;
+	}
+	if (nrow + 1 < rows && ncol - 1 >= 0) {
+		if (currGen[nrow + 1][ncol - 1] === 1) count++;
+	}
+	if (nrow + 1 < rows && ncol + 1 < cols) {
+		if (currGen[nrow + 1][ncol + 1] === 1) count++;
+	}
+	if (nrow + 1 < rows) {
+		if (currGen[nrow + 1][ncol] === 1) count++;
+	}
+	return count;
+}
+
+function createNextGen(row, col) {
+	for (row in currGen) {
+		for (col in currGen[row]) {
+			let neighbors = getNeighborCount(row, col);
+			if (currGen[row][col] === 1) {
+				if (neighbors < 2) {
+					nextGen[row][col] = 0;
+				} else if (neighbors === 2 || neighbors === 3) {
+					nextGen[row][col] = 1;
+				} else if (neighbors > 3) {
+					nextGen[row][col] = 0;
+				}
+			} else if (currGen[row][col] === 0) {
+				if (neighbors === 3) {
+					nextGen[row][col] = 1;
+				}
+			}
+		}
+	}
+}
+
+function updateCurrGen(row, col) {
     for (row in currGen) {
         for (col in currGen[row]) {
-           
-            let neighbors = getNeighborCount(row, col);
-         
-            // Check the rules
-            // If Alive
-            if (currGen[row][col] == 1) {
-              
-                if (neighbors < 2) {
-                    nextGen[row][col] = 0;
-                } else if (neighbors == 2 || neighbors == 3) {
-                    nextGen[row][col] = 1;
-                } else if (neighbors > 3) {
-                    nextGen[row][col] = 0;
-                }
-            } else if (currGen[row][col] == 0) {
-                // If Dead or Empty
-            
-                if (neighbors == 3) {
-                    // Propogate the species
-                    nextGen[row][col] = 1;// Birth?
-                }
-            }
+            currGen[row][col] = nextGen[row][col];
+            nextGen[row][col] = 0;
         }
     }
-    
-}
-function getNeighborCount(row, col) {
-    let count = 0;
-    let nrow=Number(row);
-    let ncol=Number(col);
-    
-        // Make sure we are not at the first row
-        if (nrow - 1 >= 0) {
-        // Check top neighbor
-        if (currGen[nrow - 1][ncol] == 1) 
-            count++;
-    }
-        // Make sure we are not in the first cell
-        // Upper left corner
-        if (nrow - 1 >= 0 && ncol - 1 >= 0) {
-        //Check upper left neighbor
-        if (currGen[nrow - 1][ncol - 1] == 1) 
-            count++;
-    }
-// Make sure we are not on the first row last column
-        // Upper right corner
-        if (nrow - 1 >= 0 && ncol + 1 < cols) {
-        //Check upper right neighbor
-            if (currGen[nrow - 1][ncol + 1] == 1) 
-                count++;
-        }
-// Make sure we are not on the first column
-    if (ncol - 1 >= 0) {
-        //Check left neighbor
-        if (currGen[nrow][ncol - 1] == 1) 
-            count++;
-    }
-    // Make sure we are not on the last column
-    if (ncol + 1 < cols) {
-        //Check right neighbor
-        if (currGen[nrow][ncol + 1] == 1) 
-            count++;
-    }
-// Make sure we are not on the bottom left corner
-    if (nrow + 1 < rows && ncol - 1 >= 0) {
-        //Check bottom left neighbor
-        if (currGen[nrow + 1][ncol - 1] == 1) 
-            count++;
-    }
-// Make sure we are not on the bottom right
-    if (nrow + 1 < rows && ncol + 1 < cols) {
-        //Check bottom right neighbor
-        if (currGen[nrow + 1][ncol + 1] == 1) 
-            count++;
-    }
-    
-    
-        // Make sure we are not on the last row
-    if (nrow + 1 < rows) {
-        //Check bottom neighbor
-        if (currGen[nrow + 1][ncol] == 1) 
-            count++;
-    }
-    
-    
-    return count;
-}
-    
-    function updateCurrGen() {
-       
-        for (row in currGen) {
-            for (col in currGen[row]) {
-                // Update the current generation with
-                // the results of createNextGen function
-                currGen[row][col] = nextGen[row][col];
-                // Set nextGen back to empty
-                nextGen[row][col] = 0;
-            }
-        }
-     
-    }
-function updateWorld() {
-        let cell='';
-        for (row in currGen) {
-            for (col in currGen[row]) {
-                cell = document.getElementById(row + '_' + col);
-                if (currGen[row][col] == 0) {
-                    cell.setAttribute('class', 'dead');
-                } else {
-                    cell.setAttribute('class', 'alive');
-                }
-            }
-        }
-    }
-function evolve(){
-      
-        createNextGen();
-        updateCurrGen();
-        updateWorld();
-if (started) {
-            timer = setTimeout(evolve, evolutionSpeed);
-        }
-}
-function startStopGol(){
-        let startstop=document.querySelector('#btnstartstop');
-       
-        if (!started) {
-           started = true;
-           startstop.value='Stop Reproducing';
-           evolve();
-         
-         } else {
-            started = false;
-            startstop.value='Start Reproducing';
-            clearTimeout(timer); 
-        }
-    }
- 
-  
-function resetWorld() {
-    location.reload();
 }
 
-window.onload=()=>{
-    createWorld();// The visual table
-    createGenArrays();// current and next generations
-    initGenArrays();//Set all array locations to 0=dead
+function updateWorld(row, col) {
+    let cell = "";
+    for (row in currGen) {
+        for (col in currGen[row]) {
+            cell = document.getElementById(row + "_" + col);
+            if (currGen[row][col] === 0) {
+                cell.setAttribute("class", "dead");
+            } else {
+                cell.setAttribute("class", "alive");
+            }
+        }
+    }
 }
 
-startButton.addEventListener('click', () => {
-	const test = 'sukces';
-	console.log(test);
-});
+function evolve() {
+	createNextGen();
+	updateCurrGen();
+	updateWorld();
+	if (started) {
+		timer = setTimeout(evolve, evolutionSpeed);
+	}
+}
+
+function random() {
+	let world = document.querySelector("#world");
+	let tbl = document.createElement("table");
+	tbl.setAttribute("class", "world__table");
+	tbl.setAttribute("id", "worldgrid");
+	for (let i = 0; i < rows; i++) {
+		let tr = document.createElement("tr");
+		for (let j = 0; j < cols; j++) {
+			let cell = document.createElement("td");
+			if (Math.random() > 0.5) {
+				cell.setAttribute("id", i + "_" + j);
+				cell.setAttribute("class", "alive");
+				cell.addEventListener("click", cellClick);
+			} else {
+				cell.setAttribute("id", i + "_" + j);
+				cell.setAttribute("class", "dead");
+				cell.addEventListener("click", cellClick);
+			}
+			tr.appendChild(cell);
+			let loc = cell.id.split("_");
+			let row = Number(loc[0]);
+			let col = Number(loc[1]);
+			if (cell.className === "alive") {
+				cell.setAttribute("class", "alive");
+				currGen[row][col] = 1;
+			} else {
+				cell.setAttribute("class", "dead");
+				currGen[row][col] = 0;
+			}
+		}
+		tbl.appendChild(tr);
+	}
+	world.appendChild(tbl);
+}
